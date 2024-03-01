283a9a367c87b5f605afc8d429e9ae2b8d1f97e4
System Administrator <root@Philips-MBP-2.cust.communityfibre.co.uk>
2024-03-01 23:15:15 +0000

    first commit

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -14,13 +14,19 @@ export default class IssLocationScreen extends Component {
             <TouchableOpacity onPress={() => {this.props.navigation.navigate("Home")}}>
                 <View style={styles.container}>
                     <ImageBackground style={{height: "100%"}} source={require("../assets/welcomeBg2.jpg")}>
-                        <View style={{display: "flex", width: "100%", height: "100%", alignItems: "center", position: "absolute", marginTop:70,}}>
+                        <View style={{display: "flex", width: "100%", height: "100%", alignItems: "center", position: "absolute", top:"30%"}}>
                             <Text style={{fontSize: 70}}>Welcome!</Text>
                         </View>
 
                         <View style={{display: "flex", width: "90%", height: "100%", justifyContent: 'center', alignSelf: "center"}}>
                             <Text style={{fontSize: 30,}}>
-                                I made this weather app using React Native. In the app you can search any location and it will show the weather for it
+                                I made this weather app using React Native. With the help of an API key, you can search any location and it will display the weather and change the icon
+                            </Text>
+                        </View>
+
+                        <View style={{display: "flex", position: "absolute", top:"90%", width: "100%", alignItems: "center"}}>
+                            <Text style={{opacity: 0.5}}>
+                                Click anywhere to advance to the next screen
                             </Text>
                         </View>
                     </ImageBackground>
